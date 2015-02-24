5add068fdcfe9fdc5eba2d76c93b323dec3ad742
Michał Janeczek <michal.janeczek@ymail.com>
2015-02-24 03:23:42 +0100

    Game proposal call - sort by newest

--- app/interactors/GetGameProposalsInteractor.scala
+++ app/interactors/GetGameProposalsInteractor.scala
@@ -1,9 +1,14 @@
 package interactors
 
 import DAOs.GameProposalDAO
+import com.mongodb.casbah.commons.MongoDBObject
 
 class GetGameProposalsInteractor {
   def call = {
-    GameProposalDAO.findAll().toList
+    GameProposalDAO.findAll().sort(orderBy = newest).toList
+  }
+
+  private val newest = {
+    MongoDBObject("created_at" -> -1)
   }
 }
